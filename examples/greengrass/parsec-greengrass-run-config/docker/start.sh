01f3770d6576fb74c944e48bc6566d8f1ae5b376
Urs Keller <urs@revault.ch>
2021-11-21 15:24:45 +0100

    intellij docker run and debug configurations

--- examples/greengrass/parsec-greengrass-run-config/docker/start.sh
+++ examples/greengrass/parsec-greengrass-run-config/docker/start.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e -o pipefail
 
+JAVA_OPTS=""
+case "${1}" in
+/bin/*sh)
+  exec "${1}"
+  ;;
+debug)
+  JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005"
+  echo "using Debug config ${JAVA_OPTS}"
+  ;;
+esac
+
 for mandatory_env in GG_THING_NAME GG_PROVISION GG_START; do
   if [ "${!mandatory_env}" == "" ]; then
     echo "the env variable ${mandatory_env} needs to be set"
@@ -16,15 +27,23 @@ for warn_env in AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_REGION; do
   fi
 done
 
+if [ "${GG_PROVISION}" == "true" ]; then
+  aws --region "${AWS_REGION}" iot create-thing --thing-name "${GG_THING_NAME}"
+fi
 
+if ! test -e /greengrass/config.yml; then
+  echo "please map a config file to /greengrass/config.yml"
+  exit 255
+fi
 
-java -Droot=/greengrass \
-  -jar ./lib/Greengrass.jar \
+# shellcheck disable=SC2086
+exec java ${JAVA_OPTS} -Droot=/home/ggc-user \
+  -jar /greengrass/lib/Greengrass.jar \
   --thing-name "${GG_THING_NAME}" \
   --thing-group-name GreengrassQuickStartGroup \
   --component-default-user ggc_user:ggc_group \
   --provision "${GG_PROVISION}" \
   --setup-system-service false \
   --deploy-dev-tools true \
-  --init-config ./config.yaml \
+  --init-config /greengrass/config.yml \
   --start "${GG_START}" ${GG_ADDITIONAL_CMD_ARGS}
--- examples/greengrass/parsec-greengrass-run-config/init.sh
+++ examples/greengrass/parsec-greengrass-run-config/init.sh
@@ -22,9 +22,10 @@ common_params="\
 --init-config ../config.yml \
 --trusted-plugin ../../parsec-greengrass-plugin/target/aws.greengrass.crypto.ParsecProvider.jar \
 "
-function write_run_config() {
+function write_gg_run_config() {
   name="$1"
   params="${common_params} $2"
+  echo "${name}"
   cat > ../../../../.run/${name}.run.xml << EOF
   <component name="ProjectRunConfigurationManager">
     <configuration default="false" name="${name}" type="JarApplication">
@@ -39,14 +40,152 @@ function write_run_config() {
         <env name="AWS_REGION" value="${AWS_REGION}" />
       </envs>
       <method v="2">
+        <option name="RunConfigurationTask" enabled="true" run_configuration_name="parsec_docker_run" run_configuration_type="docker-deploy" />
         <option name="Maven.BeforeRunTask" enabled="true" file="\$PROJECT_DIR$/pom.xml" goal="-o -pl examples/greengrass/parsec-greengrass-plugin package -DskipTests=true" />
       </method>
     </configuration>
   </component>
 EOF
 }
-write_run_config GreenGrassProvision "--provision true --start false"
-write_run_config GreenGrassRun "--provision false --start true"
+function write_gg_docker_run_config() {
+  name="$1"
+  provision="$2"
+  start="$3"
+  echo "${name}"
+  cat > ../../../../.run/${name}.run.xml <<EOF
+<component name="ProjectRunConfigurationManager">
+  <configuration default="false" name="${name}" type="docker-deploy" factoryName="dockerfile" server-name="Docker">
+    <deployment type="dockerfile">
+      <settings>
+        <option name="containerName" value="${name}" />
+        <option name="imageTag" value="${name}:latest" />
+        <option name="envVars">
+          <list>
+            <DockerEnvVarImpl>
+              <option name="name" value="AWS_ACCESS_KEY_ID" />
+              <option name="value" value="${AWS_ACCESS_KEY_ID}" />
+            </DockerEnvVarImpl>
+            <DockerEnvVarImpl>
+              <option name="name" value="AWS_SECRET_ACCESS_KEY" />
+              <option name="value" value="${AWS_SECRET_ACCESS_KEY}" />
+            </DockerEnvVarImpl>
+            <DockerEnvVarImpl>
+              <option name="name" value="AWS_REGION" />
+              <option name="value" value="${AWS_REGION}" />
+            </DockerEnvVarImpl>
+            <DockerEnvVarImpl>
+              <option name="name" value="AWS_REGION" />
+              <option name="value" value="${AWS_REGION}" />
+            </DockerEnvVarImpl>
+            <DockerEnvVarImpl>
+              <option name="name" value="GG_ADDITIONAL_CMD_ARGS" />
+              <option name="value" value="--trusted-plugin /provider.jar" />
+            </DockerEnvVarImpl>
+            <DockerEnvVarImpl>
+              <option name="name" value="GG_START" />
+              <option name="value" value="${start}" />
+            </DockerEnvVarImpl>
+            <DockerEnvVarImpl>
+              <option name="name" value="GG_PROVISION" />
+              <option name="value" value="${provision}" />
+            </DockerEnvVarImpl>
+            <DockerEnvVarImpl>
+              <option name="name" value="GG_THING_NAME" />
+              <option name="value" value="$(id -un)-gg-parsec" />
+            </DockerEnvVarImpl>
+          </list>
+        </option>
+        <option name="commandLineOptions" value="-v GG_PARSEC_SOCK:/run/parsec -v GG_HOME:/home/ggc_user" />
+        <option name="sourceFilePath" value="examples/greengrass/parsec-greengrass-run-config/docker/Dockerfile" />
+        <option name="volumeBindings">
+          <list>
+            <DockerVolumeBindingImpl>
+              <option name="containerPath" value="/provider.jar" />
+              <option name="hostPath" value="\$PROJECT_DIR$/examples/greengrass/parsec-greengrass-plugin/target/aws.greengrass.crypto.ParsecProvider.jar" />
+            </DockerVolumeBindingImpl>
+            <DockerVolumeBindingImpl>
+              <option name="containerPath" value="/greengrass/config.yml" />
+              <option name="hostPath" value="\$PROJECT_DIR$/examples/greengrass/parsec-greengrass-run-config/config.yml" />
+            </DockerVolumeBindingImpl>
+          </list>
+        </option>
+      </settings>
+    </deployment>
+    <method v="2">
+      <option name="RunConfigurationTask" enabled="true" run_configuration_name="parsec_docker_run" run_configuration_type="docker-deploy" />
+      <option name="Maven.BeforeRunTask" enabled="true" file="\$PROJECT_DIR$/pom.xml" goal="-o -pl examples/greengrass/parsec-greengrass-plugin package -DskipTests=true" />
+    </method>
+  </configuration>
+</component>
+EOF
+}
+
+function write_gg_docker_debug_config() {
+  debug_name="${1}"
+  cat > "../../../../.run/${debug_name} (debug).run.xml" << EOF
+<component name="ProjectRunConfigurationManager">
+  <configuration default="false" name="${debug_name} (debug)" type="Remote">
+    <module name="parsec-greengrass-plugin" />
+    <option name="USE_SOCKET_TRANSPORT" value="true" />
+    <option name="SERVER_MODE" value="false" />
+    <option name="SHMEM_ADDRESS" />
+    <option name="HOST" value="localhost" />
+    <option name="PORT" value="5005" />
+    <option name="AUTO_RESTART" value="false" />
+    <method v="2">
+      <option name="RunConfigurationTask" enabled="true" run_configuration_name="parsec_docker_run" run_configuration_type="docker-deploy" />
+      <option name="com.intellij.docker.debug.DockerBeforeRunTask" command="debug" run-config="${debug_name}" />
+    </method>
+  </configuration>
+</component>
+EOF
+}
+
+function write_parsec_docker_run_config() {
+  echo "parsec_docker_run"
+  cat > ../../../../.run/parsec_docker_run.run.xml <<EOF
+<component name="ProjectRunConfigurationManager">
+  <configuration default="false" name="parsec_docker_run" type="docker-deploy" factoryName="docker-image" server-name="Docker">
+    <deployment type="docker-image">
+      <settings>
+        <option name="imageTag" value="parallaxsecond/parsec:0.8.1" />
+        <option name="containerName" value="parsec_docker_run" />
+        <option name="commandLineOptions" value="-v GG_PARSEC_SOCK:/run/parsec -v GG_PARSEC_STORE:/var/lib/parsec/mappings" />
+      </settings>
+    </deployment>
+    <method v="2">
+      <option name="Maven.BeforeRunTask" enabled="true" file="\$PROJECT_DIR$/pom.xml" goal="-pl parsec-testcontainers package -DskipTests=true" />
+    </method>
+  </configuration>
+</component>
+EOF
+}
+
+write_parsec_docker_run_config
+
+write_gg_run_config gg_local_provision "--provision true --start false"
+write_gg_run_config gg_local_run "--provision false --start true"
+
+write_gg_docker_run_config gg_docker_provision true false
+write_gg_docker_run_config gg_docker_run false true
+
+write_gg_docker_debug_config gg_docker_provision
+write_gg_docker_debug_config gg_docker_run
+
+
+if [ "$1" == "-y" ]; then
+  delete_volumes="y"
+else
+  echo "delete volumes [y/n]?"
+  read delete_volumes
+fi
+if [ "${delete_volumes}" == "y" ]; then
+  for v in GG_PARSEC_SOCK GG_PARSEC_STORE GG_HOME; do
+    echo -n "deleting volume "
+    docker volume rm -f "${v}"
+  done
+fi
+
 
 
 
