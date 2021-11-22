b520f7ca3660b1d19b29a54fb836866dc9a74309
Urs Keller <urs@revault.ch>
2021-11-22 10:06:54 +0100

    get rid of local run config, validate, use prebuilt image

--- examples/greengrass/parsec-greengrass-run-config/init.sh
+++ examples/greengrass/parsec-greengrass-run-config/init.sh
@@ -2,60 +2,17 @@
 
 cd $(dirname "$0")
 source secrets.env
-mkdir -p device_root
-cd device_root
 
-GREENGRASS_RELEASE_VERSION=2.5.0
-GREENGRASS_ZIP_FILE=greengrass-${GREENGRASS_RELEASE_VERSION}.zip
-GREENGRASS_RELEASE_URI=https://d2s8p88vqu9w66.cloudfront.net/releases/${GREENGRASS_ZIP_FILE}
-
-if ! test -f "${GREENGRASS_ZIP_FILE}"; then
-  curl -o ${GREENGRASS_ZIP_FILE} ${GREENGRASS_RELEASE_URI}
-  unzip -o "${GREENGRASS_ZIP_FILE}"
-fi
-common_params="\
---root $(pwd) \
---thing-name $(id -un)-gg-parsec \
---thing-group-name GreengrassQuickStartGroup \
---setup-system-service false \
---deploy-dev-tools true \
---init-config ../config.yml \
---trusted-plugin ../../parsec-greengrass-plugin/target/aws.greengrass.crypto.ParsecProvider.jar \
-"
-function write_gg_run_config() {
-  params="${common_params} ${2}"
-  echo "${1}"
-  cat > ../../../../.run/${1}.run.xml << EOF
-  <component name="ProjectRunConfigurationManager">
-    <configuration default="false" name="${1}" type="JarApplication">
-      <option name="JAR_PATH" value="$(pwd)/lib/Greengrass.jar" />
-      <option name="PROGRAM_PARAMETERS" value="${params}" />
-      <option name="WORKING_DIRECTORY" value="$(pwd)" />
-      <option name="ALTERNATIVE_JRE_PATH_ENABLED" value="true" />
-      <option name="ALTERNATIVE_JRE_PATH" value="corretto-8" />
-      <envs>
-        <env name="AWS_ACCESS_KEY_ID" value="${AWS_ACCESS_KEY_ID}" />
-        <env name="AWS_SECRET_ACCESS_KEY" value="${AWS_SECRET_ACCESS_KEY}" />
-        <env name="AWS_REGION" value="${AWS_REGION}" />
-      </envs>
-      <method v="2">
-        <option name="RunConfigurationTask" enabled="true" run_configuration_name="parsec_docker_run" run_configuration_type="docker-deploy" />
-        <option name="Maven.BeforeRunTask" enabled="true" file="\$PROJECT_DIR$/pom.xml" goal="-o -pl examples/greengrass/parsec-greengrass-plugin package -DskipTests=true" />
-      </method>
-    </configuration>
-  </component>
-EOF
-}
 function write_gg_docker_run_config() {
   name="$1"
   echo "${name}"
-  cat > ../../../../.run/${name}.run.xml <<EOF
+  xpath -q -e / > ../../../.run/${name}.run.xml <<EOF
 <component name="ProjectRunConfigurationManager">
-  <configuration default="false" name="${name}" type="docker-deploy" factoryName="dockerfile" server-name="Docker">
-    <deployment type="dockerfile">
+  <configuration default="false" name="${name}" type="docker-deploy" factoryName="docker-image" server-name="Docker">
+    <deployment type="docker-image">
       <settings>
         <option name="containerName" value="${name}" />
-        <option name="imageTag" value="${name}:latest" />
+        <option name="imageTag" value="parallaxsecond/greengrass_patched:latest" />
         <option name="command" value="${2}" />
         <option name="envVars">
           <list>
@@ -107,7 +64,7 @@ EOF
 }
 
 function write_gg_docker_debug_config() {
-  cat > "../../../../.run/${1} (debug).run.xml" << EOF
+  xpath -q -e / > "../../../.run/${1} (debug).run.xml" << EOF
 <component name="ProjectRunConfigurationManager">
   <configuration default="false" name="${1} (debug)" type="Remote">
     <module name="parsec-greengrass-plugin" />
@@ -129,7 +86,7 @@ EOF
 
 function write_parsec_docker_run_config() {
   echo "parsec_docker_run"
-  cat > ../../../../.run/parsec_docker_run.run.xml <<EOF
+  xpath -q -e / > ../../../.run/parsec_docker_run.run.xml <<EOF
 <component name="ProjectRunConfigurationManager">
   <configuration default="false" name="parsec_docker_run" type="docker-deploy" factoryName="docker-image" server-name="Docker">
     <deployment type="docker-image">
@@ -148,13 +105,8 @@ EOF
 }
 
 write_parsec_docker_run_config
-
-write_gg_run_config gg_local_provision "--provision true --start false"
-write_gg_run_config gg_local_run "--provision false --start true"
-
 write_gg_docker_run_config gg_docker_provision provision
 write_gg_docker_run_config gg_docker_run run
-
 write_gg_docker_debug_config gg_docker_provision provision
 write_gg_docker_debug_config gg_docker_run run
 
@@ -171,8 +123,4 @@ if [ "${delete_volumes}" == "y" ]; then
     docker volume rm -f "${v}"
   done
 fi
-
-
-
-
 
